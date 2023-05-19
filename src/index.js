6449aa2b942ecba290e6bcdd6ab9b12420131716
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-05-19 07:39:49 +0530

    feat:add color and reset to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './scss/main.scss';
 
 const documentRoot = document.getElementById('root');
 const reactRoot = ReactDOM.createRoot(documentRoot);
