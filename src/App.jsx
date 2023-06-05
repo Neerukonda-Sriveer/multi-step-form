c4b0c4e92ae842317cae7e8a251130f83a821005
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-06-05 06:17:09 +0530

    feat:add heading and subtitle typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 export default function App(){
     return(
-        <h1>hello world</h1>
+        <>
+            <h1 className="heading">Personal info</h1>
+            <h2 className="subtitle">Please provide your name, email address and phone number.</h2>
+        </>
     )
 }
