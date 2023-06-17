d7edb3ce7fa5db655094bab5f052c1ba489a8064
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-06-17 15:41:10 +0530

    feat:thank you page

--- src/components/pages/ThankYou.jsx
+++ src/components/pages/ThankYou.jsx
@@ -1,9 +1,14 @@
+import ThankYouImg from '../../images/icon-thank-you.svg'
+import Classes from './ThankYou.module.scss'
+
 export default function ThankYou({
     className
 }){
     return(
-        <div className={className}>
-            <h1>ThankYou</h1>
+        <div className={`${className} ${Classes['page']}`}>
+            <img className={`${Classes['img']}`} src={ThankYouImg} alt="thank you" />
+            <h1 className={`${Classes['heading']}`}>Thank you!</h1>
+            <p className={`${Classes['paragraph']}`}>Thanks for confirming your subscription! we hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
         </div>
     )
 }
